f66daec98be7dec3b804c87f988fe9fea8100bd7
9um4 <51898564+9um4@users.noreply.github.com>
2024-06-24 23:46:32 +0900

    feat: Update app.py to pass dynamic data to index.html template
    Modified the main route in app.py to pass dynamic title and message data to the index.html template.
    
    - Added title and message variables
    - Passed title and message to render_template
    - Included commented example of using a context dictionary for data

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return render_template('index.html')
+    title: str = "Hello, Flask with HTML!"
+    message: str = "This is my first Flask app with HTML!"
+    return render_template('index.html', title=title, message=message)
+
+    """
+    매개변수를 지정해 주는 것 대신 dictionary를 사용할 수 있음
+    
+    context = {
+        'title': 'Hello, Flask with HTML!',
+        'message': 'This is my first Flask app with HTML!'
+    }
+    return render_template('index.html', **context)
+    """
 
 if __name__ == "__main__":
     app.run(debug=True)
